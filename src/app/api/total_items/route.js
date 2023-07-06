ad8d2b4da44fa939d5ffb68e7386ae3a3912c3c5
Sadaf Saiyed <114169478+Sadafsaiyed@users.noreply.github.com>
2023-07-06 22:22:10 +0530

    Add files via upload

--- src/app/api/total_items/route.js
+++ src/app/api/total_items/route.js
@@ -7,10 +7,11 @@ export const GET = async (req) => {
     const enddate = searchParams.get("enddate");
     const department = searchParams.get("department");
 
+    
+
     let lseat = 0;
-    const returndataarray = data.map((obj)=>{
-        
-        if(Date.parse(obj.date.split(" ")[0])>Date.parse(startdate) && Date.parse(obj.date.split(" ")[0])<Date.parse(enddate) && obj.department==department )
+    data.map((obj)=>{
+        if(Date.parse(obj.date.split(" ")[0])>Date.parse(startdate) && Date.parse(obj.date.split(" ")[0])<Date.parse(enddate) && obj.department==department)
         { 
             lseat =lseat + obj.seats
         }
